3abee5bb250d32dcf1fdd38c380cee264cdb9746
hardtrip <user@hardtrip.ca>
2025-03-05 13:19:25 -0500

    Some form of sequencer
    Division is still not correct, but I now understand how to setup loops and stuff. Will keep figuring it out!

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,6 +1,8 @@
 "use strict";
 //TEMP Script to test things out
+let playState = false
 
+const timekeeper = Tone.getTransport();
 
 //Keyboard
 const keyboard = new AudioKeys({
@@ -23,22 +25,32 @@ const synth2 = new Tone.Synth({
 const instrumentList = [
     synth1,
     synth2
-]
+];
 
-let currentSynth = synth1
+const patternList = [
+    ["C5", "B5", "C5"],
+    ["D3","D4","D5","D4"],
+];
 
+let currentSynth = instrumentList[0];
 
+let currentBPM = 120.0;
 
+let currentPattern = patternList[0];
+
+let currentTransportTime = updateTransportTime();
+
+let track1 = new Tone.Loop((time) => {
+    playNote(time);
+}, currentTransportTime).start(0);;
+let currentNote = 0;
 //User Activated input
 window.onload = function() {
+    Tone.start();
+    changePattern(0);
     const testButton = document.getElementById("test-button");
 
-    testButton.addEventListener("click", () => {
-        if(Tone.context.state != "running"){
-           Tone.start();
-       }
-       currentSynth.triggerAttackRelease("C4", "8n");
-    });    
+    testButton.addEventListener("click", playButton);  
 }
 
 keyboard.down((key) => {
@@ -47,10 +59,44 @@ keyboard.down((key) => {
 })
 
 
+function updateTransportTime(){
+    return `${currentPattern.length}n`;
+}
 
+function playButton(){
+    if (playState){
+        Tone.getTransport().stop();
+        playState = false;
+    }
+    else{
+        console.log(track1)
+        playState = true;
+        Tone.getTransport().start();
+    }
+   // currentSynth.triggerAttackRelease("C4", "8n");
+
+}
+
+function playNote(time){
+    if (currentNote >= currentPattern.length){
+        currentNote = 0;
+    }
+    currentSynth.triggerAttackRelease(currentPattern[currentNote], "8n", time);
+    currentNote++;
+}
 
 
 //Function called by HTML
 function changeInstrument(id){
     currentSynth = instrumentList[id];
 }
+
+function changePattern(id){
+    currentPattern = patternList[id];
+    currentTransportTime = updateTransportTime();
+    currentNote = 0;
+    track1 = new Tone.Loop((time) => {
+        playNote;
+    }, currentTransportTime).start(0);
+}
+
